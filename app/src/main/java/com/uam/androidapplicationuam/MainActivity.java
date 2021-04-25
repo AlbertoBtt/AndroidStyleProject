94bf264de7fdb2c70c3d949e535eb1611142d3b1
AlbertoBtt <54907618+AlbertoBtt@users.noreply.github.com>
2021-04-25 21:29:43 +0200

    Many things

--- app/src/main/java/com/uam/androidapplicationuam/MainActivity.java
+++ app/src/main/java/com/uam/androidapplicationuam/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
     public void continue1(View v){
         setContentView(R.layout.activity_main2);
     }
@@ -30,10 +40,9 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "All Is Tested!!!!!", Toast.LENGTH_SHORT).show();
     }
 
-    public void settings(View v){
+    public void settings(MenuItem item){
         Intent intent1 = new Intent(this, SettingsActivity.class);
         startActivity(intent1);
-        //setContentView(R.layout.settings);
     }
 
     public void return_settings(View v){
